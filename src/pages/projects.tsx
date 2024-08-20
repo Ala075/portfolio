54f893c3cfaf17574a39e0dcb11987bfbbcdec46
Ala075 <ala.arfaoui751@gmail.com>
2024-08-20 13:27:45 +0100

    update projects section

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,28 +1,28 @@
 import Data from "../../data/db.json";
-import Card from '../components/cardProject';
+import Card from "../components/cardProject";
 
 const Projects = () => {
-    const { projects } = Data;
-    
-    return (
-        <section className="projects section" id="projects">
-            <h2 className="section__title-1">
-                <span>Projects</span>
-            </h2>
-            <div className="projects__container container grid">
-                {projects.map((project, index) => (
-                    <Card
-                        key={index}
-                        image={project.image}
-                        subtitle={project.subtitle}
-                        title={project.title}
-                        description={project.description}
-                        links={project.links}
-                    />
-                ))}
-            </div>
-        </section>
-    );
+  const { projects } = Data;
+
+  return (
+    <section className="projects section" id="projects">
+      <h2 className="section__title-1">
+        <span>Projects</span>
+      </h2>
+      <div className="projects__container container grid">
+        {projects.map((project, index) => (
+          <Card
+            key={index}
+            image={project.image}
+            subtitle={project.subtitle}
+            title={project.title}
+            description={project.description}
+            links={project.links}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
