9f9042f4c241ca12a4a894060eff8d656a06ad91
Ala075 <ala.arfaoui751@gmail.com>
2024-08-25 12:02:32 +0100

    fixing some issues

--- src/components/cardProject.tsx
+++ src/components/cardProject.tsx
@@ -1,54 +1,50 @@
-
 interface CardProps {
-    image: string;
-    subtitle: string ;
-    title: string;
-    description: string;
-    links: { 
-        github: string; 
-        github2: string; 
-    };
+  image: string;
+  subtitle: string;
+  title: string;
+  description: string;
+  links: {
+    github: string;
+    github2: string;
+  };
 }
 
-
 function Card({ image, subtitle, title, description, links }: CardProps) {
-    
-    const displayShortDescription = () => {
-        return description.length > 100 ? description.substring(0, 100) + "..." : description;
-    }
+  const displayShortDescription = () => {
+    return description.length > 100
+      ? description.substring(0, 100) + "..."
+      : description;
+  };
 
-    return(
-        <div className="projects__card">
-              <div className="projects__image">
-                  <img src={image} alt="image" className="projects__img" loading="lazy" />
-                   <a href="https://alaarfaoui.vercel.app/" className="projects__button button">
-                       <i className="ri-arrow-right-up-line"></i>
-                    </a>
-              </div>
-                    
-              <div className="projects__content">
-                  <h3 className="projects__subtitle">
-                      {subtitle}
-                  </h3>
-                  <h2 className="projects__title">
-                      {title}
-                  </h2>
-                        
-                  <p className="projects__description">
-                      {displayShortDescription()}
-                  </p>
-              </div>
-              
-              <div className="projects__buttons">
-                  <a href={links.github} target="_blanc" className="projects__link">
-                      <i className="ri-github-line"></i>View
-                  </a>
-                        
-                  <a href={links.github2} target="_blanc" className="projects__link">
-                      <i className="ri-github-line"></i>View
-                  </a>
-            </div>
-        </div>
-    );
+  return (
+    <div className="projects__card">
+      <div className="projects__image">
+        <img src={image} alt="image" className="projects__img" loading="lazy" />
+        <a
+          href={links.website}
+          className="projects__button button"
+        >
+          <i className="ri-arrow-right-up-line"></i>
+        </a>
+      </div>
+
+      <div className="projects__content">
+        <h3 className="projects__subtitle">{subtitle}</h3>
+        <h2 className="projects__title">{title}</h2>
+
+        <p className="projects__description">{displayShortDescription()}</p>
+      </div>
+
+      <div className="projects__buttons">
+        <a href={links.github} target="_blanc" className="projects__link">
+          <i className="ri-github-line"></i>View
+        </a>
+
+        <a href={links.github2} target="_blanc" className="projects__link">
+          <i className="ri-github-line"></i>View
+        </a>
+      </div>
+    </div>
+  );
 }
 export default Card;
